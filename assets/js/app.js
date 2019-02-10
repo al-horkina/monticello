7115425fa470c7eeeaa5af191b98d74573f3cb64
Alexandra Horkina <al.horkina@gmail.com>
2019-02-10 14:01:00 +0200

    add news section

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+(function ($) {
+    $(document).ready(function () {
+        $('.news__slider').slick({
+            infinite: true,
+            centerMode: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 4000
+        });
+    });
+})(jQuery);
