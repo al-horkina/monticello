69ad701c4627ac86f2cd7ad9c80087c4ddf77ba1
Alexandra Horkina <al.horkina@gmail.com>
2019-02-10 20:50:06 +0200

    add footer section

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,5 +11,57 @@
         });
 
         $(".fancybox").fancybox();
+
+        const $map = $('.location');
+        const map = new google.maps.Map(
+            $map[0],
+            {
+                center: {lat: 40.659, lng: -73.903},
+                zoom: 14,
+                styles: [
+                    {
+                        "featureType": "road",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "elementType": "labels.text",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    }
+                ]
+            }
+        );
+
+        const image = {
+            url: '../img/marker.png',
+            scaledSize: new google.maps.Size(64, 64)
+        };
+
+        const marker = new google.maps.Marker({
+            position: {lat: 40.681, lng: -73.918},
+            map: map,
+            title: '91 Nolan Extensions Suite 670',
+            icon: image
+        });
+
+
     });
 })(jQuery);
